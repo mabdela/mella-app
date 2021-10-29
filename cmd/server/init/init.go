e84270fb4556eab6314d5094d0405e8e788ffcfa
samuael <samuaelb@umich.edu>
2021-10-29 22:53:49 +0300

    init method

--- cmd/server/init/init.go
+++ cmd/server/init/init.go
@@ -2,6 +2,12 @@
 package main
 
 import (
+	"context"
+
+	"github.com/mabdela/mella-backend/pkg/constants/model"
+	"github.com/mabdela/mella-backend/pkg/constants/state"
+	"github.com/mabdela/mella-backend/pkg/storage/mongodb"
+	"github.com/mabdela/mella-backend/platforms/helper"
 	"github.com/subosito/gotenv"
 )
 
@@ -10,5 +16,19 @@ func init() {
 }
 
 func main() {
+	connect := mongodb.ConnectMongoDB()
+	admin := &model.Admin{
+		Email:      "samuaeladnew@gmail.com",
+		Firstname:  "samuael",
+		Lastname:   "adnew ",
+		Password:   "$2a$10$TQW6xnGA6xntDzWU21fGg.7.ad8vgfWz7OYvQETIRD/BPrpuvUKvG",
+		Superadmin: true,
+	}
+
+	insert, _ := connect.Collection(state.ADMINS).InsertOne(context.TODO(), admin.GetInsertAdmin())
+	oidstring := helper.ObjectIDFromInsertResult(insert)
+	admin.ID = oidstring
+
+	print(string(helper.MarshalThis(admin)))
 
 }
