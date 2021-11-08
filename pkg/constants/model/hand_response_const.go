4273a920f803ebe247245cfe9d249f225351f2dd
Nigatu <78375999+jeno1990@users.noreply.github.com>
2021-11-08 09:11:29 +0300

    message naming fixed

--- pkg/constants/model/hand_response_const.go
+++ pkg/constants/model/hand_response_const.go
@@ -5,27 +5,27 @@ package model
 // AdminLoginResponse to be usedby the admin response class
 type AdminLoginResponse struct {
 	Success bool   `json:"success"`
-	Message string `json:"message"`
+	Message string `json:"msg"`
 	Admin   *Admin `json:"admin"`
 }
 
 // LoginResponse to be usedby the admin response class
 type LoginResponse struct {
 	Success bool        `json:"success"`
-	Message string      `json:"message"`
+	Message string      `json:"msg"`
 	User    interface{} `json:"user"`
 }
 
 // CreateUserResponse ...
 type CreateUser struct {
 	Success bool   `json:"success"`
-	Message string `json:"message"`
+	Message string `json:"msg"`
 }
 
 // SimpleSuccessNotifier ...
 type SimpleSuccessNotifier struct {
 	Success bool   `json:"success"`
-	Message string `json:"message"`
+	Message string `json:"msg"`
 }
 
 // ShortSuccess
